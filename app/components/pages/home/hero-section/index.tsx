78a4f08435788be0e80a913c6b4197f0dff83a76
Italo <italodopeey@gmail.com>
2023-06-06 18:43:34 -0300

    style: mudando algumas classes

--- app/components/pages/home/hero-section/index.tsx
+++ app/components/pages/home/hero-section/index.tsx
@@ -40,7 +40,7 @@ export default function HeroSection() {
   ]  
 
   return (
-    <section className="w-full h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col
+    <section className="w-full lg:h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col
     justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px]">
       <div className="container flex items-start justify-between flex-col-reverse lg:flex-row">
         <div className="w-full lg:max-w-[530px]">
@@ -60,7 +60,7 @@ export default function HeroSection() {
             ))}
           </div>
           <div className="mt-6 lg:mt-10 flex sm:items-center sm:gap-5 flex-col sm:flex-row">
-            <Button className="shadow-button" >
+            <Button className="w-max shadow-button" >
               Entre em contato
               <HiArrowNarrowRight size={18} />
             </Button>
@@ -82,9 +82,9 @@ export default function HeroSection() {
         <Image 
           width={420}
           height={404}
-
           src='/images/profile-pic.png'
           alt="Foto de perfil do gobriel borges"
+          className="w-[300px] h-[300px] lg:w-[420px] lg:h-[404px] mb-6 lg:mb-0 shadow-2xl rounded-lg object-cover"
         />
       </div>
     </section>
