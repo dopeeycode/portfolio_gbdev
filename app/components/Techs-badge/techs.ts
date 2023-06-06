23419ee7228b3877a76884885f144800949315f0
Italo <italodopeey@gmail.com>
2023-06-06 12:07:42 -0300

    feat: componente techs com dados em arquivo ts

---
+++ app/components/Techs-badge/techs.ts
@@ -0,0 +1,26 @@
+export const techs = [
+  {
+    id: 1,
+    name: 'Next.js'
+  },
+  {
+    id: 2,
+    name: 'Next Auth'
+  },
+  {
+    id: 3,
+    name: 'Stitches'
+  },
+  {
+    id: 4,
+    name: 'Radix'
+  },
+  {
+    id: 5,
+    name: 'Prisma'
+  },
+  {
+    id: 6,
+    name: 'React Query'
+  },
+]
